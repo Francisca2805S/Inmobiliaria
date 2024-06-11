e4f3cec26d8dcc102cb4f6797667b49eaa2ce978
Francisca2805S <francisca.santillana2128@gmail.com>
2024-06-10 22:30:57 -0400

    algunas correcciones

--- inmobiliaria/settings.py
+++ inmobiliaria/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'inmobiliaria.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'inmobiliaria',
+        'NAME': 'Inmobiliaria',
         'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST': '127.0.0.1',
